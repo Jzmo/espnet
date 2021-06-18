e34be6b449d9538f17ca64bfe887c0e1bccb2de4
Tianzi Wang <jzmo@br014.ib.bridges2.psc.edu>
2021-06-18 12:37:49 -0400

    add km

--- egs2/librispeech/hubert_asr1/run_pretrain.sh
+++ egs2/librispeech/hubert_asr1/run_pretrain.sh
@@ -13,7 +13,7 @@ asr_config=conf/tuning/train_asr_hubert_base_960h_pretrain.yaml
 lm_config=conf/tuning/train_lm_transformer2.yaml
 inference_config=conf/decode_asr.yaml
 
-sh ./km.sh 50
+sh ./local/km.sh 50
 
 ./hubert_asr.sh \
     --lang en \
